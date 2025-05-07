776462b3893f74869e2190e2a9eb05894d3c7936
Bhrigu Srivastava <captain.bhrigu@gmail.com>
2025-05-06 19:04:01 -0700

    Initial Commit

---
+++ TimeCraft/TimeCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCraftApp.swift
+//  TimeCraft
+//
+//  Created by Bhrigu Srivastava on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeCraftTests/TimeCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCraftTests.swift
+//  TimeCraftTests
+//
+//  Created by Bhrigu Srivastava on 5/6/25.
+//
+
+import Testing
+@testable import TimeCraft
+
+struct TimeCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
